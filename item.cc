6fe66104b25e5a688e640d4e34538d395c6f242a
s1260177_Yuki Sakurai <52778944+s1260177@users.noreply.github.com>
2020-11-19 16:06:14 +0900

    メニュー画面の制作

--- item.cc
+++ item.cc
@@ -15,30 +15,22 @@ Date:2020-11
 
 #include<fstream>//infile outfile
 
-using std::cin;
-using std::cout;
-using std::endl;
-
-using std::string;
-using std::vector;
-using std::list;
-using std::istream;
-using std::setw;
+using namespace std;
 
 
 class Item_info{
 public:
   // インターフェース
   Item_info();
-  Item_info( std::istream& );
+  Item_info( istream& );
   string id() const { return ID; } ;
   string name() const { return Name; } ;
   string date() const { return Date; } ;
   string price() const { return Price; };
   int num() const{return Num;};
   vector<string>position()const{return Position;};
-  bool valid() const { return !Position.empty(); };
-  std::istream& read(std::istream&);
+  bool valid() const { return !Position.empty();};
+  istream& read(istream&);
 
 
 private:
@@ -50,12 +42,60 @@ private:
   vector<string> Position;//商品の売り場の位置のvector
 };
 
-int main(int argc, char* argv[]){
+// メンバ関数の定義
+Item_info::Item_info( istream& is) { read(is); }
 
+
+istream& Item_info::read(istream& is ){
+
+  return is;
+}
+
+int main(int argc, char* argv[]){
+  //ファイル操作
+  std::ifstream infile(argv[1]);
+  std::ofstream outfile(argv[2]);
   if(argc!=3){
     std::cerr<<"Error"<<endl;
     return -1;
   }
-  
+
+  //入力
+  /*
+  vector<Item_info> items;
+  Item_info record;
+
+  while(record.read(infile)){//cause of error
+    items.push_back(record);
+  }
+*/
+  //入力完了表示
+  cout<<"データ入力完了"<<endl;
+  //売り場注意情報表示
+
+
+  //メニュー表示
+  cout<<"メニューを選んでください"<<endl;
+  int menu_select=0;
+  while(menu_select!=4){
+    cout<<"1.商品情報の追加"<<endl<<"2.商品検索"<<endl<<"3.売り場検索"<<endl<<"4.終了（データ更新)"<<endl;
+    cin>>menu_select;
+    if(menu_select==1){
+
+    }else if(menu_select==2){
+
+    }else if (menu_select==3){
+
+    }else if(menu_select==4){
+
+    }else cout<<"メニュー番号を入力してください。"<<endl;
+  }
+
+  //終了動作
+  cout<<"ファイル更新中"<<endl;
+  //outfile<<;
+
+  cout<<"完了しました。終了します"<<endl;
+
   return 0;
 }
